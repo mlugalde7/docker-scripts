083e8036b9e952473586dc1a3c2ebc6eab87c1a2
Esteban Chamberlain <esteban@kapiwebdeb.com>
2018-02-12 13:47:34 -0600

    change the repository name to kapiwebdev

--- scripts/common/functions.sh
+++ scripts/common/functions.sh
@@ -169,10 +169,10 @@ get_docker_image() {
 
     case "$type" in
 
-        lamp) echo 'adriancr/lamp'
+        lamp) echo 'kapiwebdev/lamp'
 #        lamp) echo 'adriancr/lamp'#
             ;;
-        magento1) echo 'adriancr/lamp'
+        magento1) echo 'kapiwebdev/lamp'
             ;;
         magento2) echo 'kapiwebdev/lamp2'
             ;;
